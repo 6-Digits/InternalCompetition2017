abf21753bccf8cd55f3cd3103fd074180c0b53eb
thefireblade <jhuang6625@bths.edu>
2017-09-09 13:47:57 -0400

    First commit

---
+++ title.js
@@ -0,0 +1,11 @@
+function popUp(){
+	while(1==1){
+		window.open("http://www.urbandictionary.com/define.php?term=spamming");
+	}
+}
+function trollAlert(){
+	while(1==1){
+		alert("You are dumb");
+	}
+}
+
